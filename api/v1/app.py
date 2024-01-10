4937e48ddd6b3e9243863e3d90539f87abd25b9a
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-10 13:58:19 +0000

    Fixed circular import

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,12 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
 
 @app.teardown_appcontext
 def tear_down():
@@ -22,4 +23,4 @@ def tear_down():
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = os.environ.get("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
