4cca26c3ea714ff6a848a37124a2ead810ce49cd
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-11 22:06:31 +0000

    Task7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,6 +3,7 @@
     Route for state objects
 """
 from models.city import City
+from models.state import State
 from models import storage
 from flask import abort, jsonify, request, make_response
 from api.v1.views import app_views
@@ -10,12 +11,12 @@ from api.v1.views import app_views
 
 @app_views.route("/states/<state_id>/cities")
 @app_views.route("/cities/<city_id>")
-def retrieve_states(state_id=None, city_id=None):
-    """Return all state object"""
+def retrieve_cities(state_id=None, city_id=None):
+    """Return city object"""
     states = storage.all(State).values()
     if state_id:
         objList = [obj for obj in states if state_id == obj.id]
-        if objList
+        if objList:
             cities = objList.cities
             cities = [obj.to_dict for obj in cities]
             return jsonify(cities)
